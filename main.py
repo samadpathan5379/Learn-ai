bb68c350ae75ceceae11ef243e0be538134bd477
Samad pathan <112460400+samadpathan5379@users.noreply.github.com>
2025-12-14 23:08:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 # --- SETUP GEMINI ---
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
 # --- SETUP DISCORD ---
 intents = discord.Intents.default()
